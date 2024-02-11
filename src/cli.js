bb241f9b1f0efb046d1f47216cff1c6f62a1fc4b
ElenaSamosudova <elenaaleksandrovskaya@Elenas-MacBook-Air.local>
2024-02-11 20:41:54 +0300

    Brain-games_Step-4_fix-linter-errors

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,7 @@
 import readlineSync from 'readline-sync';
 
-const name = (userName) => { // eslint-disable-next-line no-param-reassign
-  userName = readlineSync.question('May I have your name? ');
+const name = () => {
+  const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
 };
 export default name;
