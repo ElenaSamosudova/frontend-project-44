5e4e17386aff5da4b77b353671a624f3b7d7770f
ElenaSamosudova <elenaaleksandrovskaya@Elenas-MacBook-Air.local>
2024-02-13 20:58:56 +0300

    Brain-games_Step-5_Brain-even_attempt-3

--- src/helloEven.js
+++ src/helloEven.js
@@ -1,7 +1,7 @@
 import readlineSync from 'readline-sync';
 
+export const userName = readlineSync.question('May I have your name? ');
 const name = () => {
-  const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!\nAnswer "yes" if the number is even, otherwise answer "no".`);
 };
 export default name;
--- src/indexEven.js
+++ src/indexEven.js
@@ -1,19 +1,26 @@
 import readlineSync from 'readline-sync';
-import name from './helloEven.js';
+import { userName } from './helloEven.js';
 import getRandomInt from './randomEven.js';
+import name from './helloEven.js';
 
 console.log('Welcome to the Brain Games!');
 name();
 
 const nextNum = () => {
-  const num = getRandomInt(999);
+let num = getRandomInt(99);
   console.log(`Question: ${num}`);
   const answer = readlineSync.question('Your answer: ');
-  if (num % 2 === 0 && answer === 'yes') {
-    console.log('Correct!');
-    nextNum();
+  if (num % 2 === 0 && answer === 'no') {
+    console.log(`'${answer}' is wrong answer ;(. Correct answer was \'no\'.\nLet\'s try again, ${userName}!`);
   }
-  return `'yes' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, ${name}!`;
+  else if (num % 2 === 0 && answer === 'yes') {
+  console.log(`Correct!`);
+}
+else {
+    console.log(`'${answer}' is wrong answer ;(. Correct answer was \'no\'.\nLet\'s try again, ${userName}!`);
+}
+nextNum();
 };
 
 export default nextNum;
+nextNum();
