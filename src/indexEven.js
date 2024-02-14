6c59d7766d6fc8280adec0f71b0630a7adb7421f
ElenaSamosudova <elenaaleksandrovskaya@Elenas-MacBook-Air.local>
2024-02-14 18:25:11 +0300

    Brain-games_Step-5_Brain-even_success

--- src/indexEven.js
+++ src/indexEven.js
@@ -7,20 +7,29 @@ console.log('Welcome to the Brain Games!');
 name();
 
 const nextNum = () => {
-let num = getRandomInt(99);
-  console.log(`Question: ${num}`);
-  const answer = readlineSync.question('Your answer: ');
-  if (num % 2 === 0 && answer === 'no') {
-    console.log(`'${answer}' is wrong answer ;(. Correct answer was \'no\'.\nLet\'s try again, ${userName}!`);
+  let i = 1;
+  while (i <= 3) {
+    let num = getRandomInt(99);
+
+    console.log(`Question: ${num}`);
+    const answer = readlineSync.question('Your answer: ');
+    if (num % 2 === 0 && answer === 'no') {
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was \'no\'.\nLet\'s try again, ${userName}!`);
+      break;
+    } else if (num % 2 === 0 && answer === 'yes') {
+      console.log(`Correct!`);
+    } else {
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was \'no\'.\nLet\'s try again, ${userName}!`);
+      break;
+    }
+    i += 1;
   }
-  else if (num % 2 === 0 && answer === 'yes') {
-  console.log(`Correct!`);
+
+  if (i > 3) {
+    console.log(`Congratulations, ${userName}!`);
+  }
+
 }
-else {
-    console.log(`'${answer}' is wrong answer ;(. Correct answer was \'no\'.\nLet\'s try again, ${userName}!`);
-}
-nextNum();
-};
 
 export default nextNum;
 nextNum();
